cfd5d2f1ab517cddaabddf85f24888a491ce035d
JosunLP <jolun.swtor@gmail.com>
2020-04-25 17:48:29 +0200

    .

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -28,7 +28,7 @@ const siteConfig = {
     baseUrl: '/', // Base URL for your project */
 
     // Used for publishing and more
-    projectName: 'WAPS Framework',
+    projectName: 'WAPS-Framework',
     organizationName: 'WAPS Framework Team',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
